c562dacd06e34350abc5cb02665bf9f3a05feb88
Issa <issa.diallo@lemonlearning.fr>
2022-08-22 00:34:36 +0200

    :wrench: add sequelize and connect our rest api fix #15

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,33 @@
 import express from 'express'
 import config from 'config'
 import logger from './utils/logger'
+import { Sequelize } from 'sequelize'
 
 const port = config.get<number>('port')
-
 const app = express()
+
+const MYSQL_HOST = config.get<string>('MYSQL_HOST')
+const MYSQL_USER = config.get<string>('MYSQL_USER')
+const MYSQL_PASSWORD = config.get<string>('MYSQL_PASSWORD')
+const MYSQL_DATABASE = config.get<string>('MYSQL_DATABASE')
+const MYSQL_PORT = config.get<number>('MYSQL_PORT')
+
+const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {
+  host: MYSQL_HOST,
+  dialect: 'mysql',
+  port: MYSQL_PORT,
+})
+
+const connexion = async () => {
+  try {
+    await sequelize.authenticate()
+    logger.info('Connection has been established successfully.')
+  } catch (error) {
+    logger.fatal('Unable to connect to the database:', error)
+  }
+}
+
+connexion()
 
 app.listen(port, () => {
   logger.info(`Listening on port at http://localhost:${port}`)
