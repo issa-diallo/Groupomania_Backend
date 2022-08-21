d3b529ddc17f4b8b48da9f04465b523e7cc227b6
Issa <issa.diallo@lemonlearning.fr>
2022-08-21 13:10:32 +0200

    :rotating_light: Added eslint and prettier fix #4

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import config from 'config';
-import logger from './utils/logger';
+import express from 'express'
+import config from 'config'
+import logger from './utils/logger'
 
-const port = config.get<number>('port');
+const port = config.get<number>('port')
 
-const app = express();
+const app = express()
 
 app.listen(port, () => {
-  logger.info(`Listening on port at http://localhost:${port}`);
-});
+  logger.info(`Listening on port at http://localhost:${port}`)
+})
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,13 @@
 import pino from 'pino'
 
 const log = pino({
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
-            ignore: 'pid,hostname',
-        }
-    }
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+      ignore: 'pid,hostname',
+    },
+  },
 })
 
 export default log
