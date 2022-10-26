ec10a37a0ec95c1da9eda55eba9d1cdb6a4dca06
Issa <issa.diallo@lemonlearning.fr>
2022-10-26 23:09:49 +0200

    :wrench: fix serve file static

--- src/utils/helpers/constants.ts
+++ src/utils/helpers/constants.ts
@@ -1,13 +1,7 @@
 import { resolve } from 'path'
 
 const ROOT_DIR = resolve(__dirname, '..', '..', '..')
-export const RELATIVE_UPLOAD_PROFIL_PATH = 'src/clients/uploads/profil/'
-export const ABSOLUTE_UPLOAD_PROFIL_PATH = resolve(
-  ROOT_DIR,
-  'src/clients/uploads/profil/'
-)
-export const RELATIVE_UPLOAD_POST_PATH = 'src/clients/uploads/post/'
-export const ABSOLUTE_UPLOAD_POST_PATH = resolve(
-  ROOT_DIR,
-  'src/clients/uploads/post/'
-)
+export const RELATIVE_UPLOAD_PROFIL_PATH = 'images/profil/'
+export const ABSOLUTE_UPLOAD_PROFIL_PATH = resolve(ROOT_DIR, 'images/profil/')
+export const RELATIVE_UPLOAD_POST_PATH = 'images/post/'
+export const ABSOLUTE_UPLOAD_POST_PATH = resolve(ROOT_DIR, 'images/post/')
