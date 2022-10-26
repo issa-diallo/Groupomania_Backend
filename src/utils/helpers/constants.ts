8945839a2aa54abaafcd3febe2b3ff649018c095
Issa <issa.diallo@lemonlearning.fr>
2022-10-27 01:12:16 +0200

    :wrench: Fix path upload profil

--- src/utils/helpers/constants.ts
+++ src/utils/helpers/constants.ts
@@ -1,7 +1,10 @@
 import { resolve } from 'path'
 
 const ROOT_DIR = resolve(__dirname, '..', '..', '..')
-export const RELATIVE_UPLOAD_PROFIL_PATH = 'images/profil/'
-export const ABSOLUTE_UPLOAD_PROFIL_PATH = resolve(ROOT_DIR, 'images/profil/')
+export const RELATIVE_UPLOAD_PROFIL_PATH = 'src/images/profil/'
+export const ABSOLUTE_UPLOAD_PROFIL_PATH = resolve(
+  ROOT_DIR,
+  'src/images/profil/'
+)
 export const RELATIVE_UPLOAD_POST_PATH = 'images/post/'
-export const ABSOLUTE_UPLOAD_POST_PATH = resolve(ROOT_DIR, 'images/post/')
+export const ABSOLUTE_UPLOAD_POST_PATH = resolve(ROOT_DIR, 'src/images/post/')
