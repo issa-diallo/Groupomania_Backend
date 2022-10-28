a1fe722db892b1f07c9d31358ea8bd9c1580dd8f
Issa <issa.diallo@lemonlearning.fr>
2022-10-28 18:53:03 +0200

    :wrench: Fix test user

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,26 +3,31 @@ import { RequestAuth, TokenInterface } from '../authentification/types'
 import jwt from 'jsonwebtoken'
 
 export const auth = (req: RequestAuth, res: Response, next: NextFunction) => {
-  try {
-    // Get the token from the request header.
-    const token = req.headers.authorization.split(' ')[1]
-
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: 'you did not provide an authentication token' })
-    } else {
-      // Verify the token.
-      const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
-      // We extract the user ID of our token
-      const userId = (decoded as TokenInterface).userId
-      // Add the userId to the request object.
-      req.auth = {
-        userId: userId,
-      }
-    }
-    next()
-  } catch (error) {
-    res.status(500).json({ error })
+  let token
+  // Get the token from the request header.
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === 'Bearer'
+  ) {
+    token = req.headers.authorization.split(' ')[1]
+  } else {
+    return res.status(401).json({
+      message: 'you did not provide an authentication token',
+      name: 'Access denied',
+    })
   }
+
+  if (token) {
+    // Verify the token.
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+    // We extract the user ID of our token
+    const userId = (decoded as TokenInterface).userId
+    // Add the userId to the request object.
+    req.auth = {
+      userId: userId,
+    }
+  } else {
+    return res.status(500)
+  }
+  next()
 }
