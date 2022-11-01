24c2fec2751157e5a220bbeaa38db8ccfc7d5380
Issa <issa.diallo@lemonlearning.fr>
2022-11-01 17:33:41 +0100

    :wrench: Fix function 401

--- src/authentification/userNotAllowed.ts
+++ src/authentification/userNotAllowed.ts
@@ -6,12 +6,12 @@ export const userAllowedOr401 = (
   reqUserId: number,
   res: Response
 ) => {
-  if (user == null) {
+  if (user == null || user === undefined) {
     return res.status(401).json({ message: 'user does not exist ' })
   }
   if (user.id !== reqUserId) {
-    return res
-      .status(401)
-      .json({ message: 'User is not authorized to access this resource' })
+    return res.status(401).json({
+      message: 'User is not authorized to access this resource',
+    })
   }
 }
